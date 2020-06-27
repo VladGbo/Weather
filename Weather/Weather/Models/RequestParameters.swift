7177dfd37860dea6cd112300dc9fcaeaef34d906
Vladislav Horbenko <vladgbo@gmail.com>
2020-06-27 17:48:22 +0300

    Finish setup configuration

---
+++ Weather/Weather/Models/RequestParameters.swift
@@ -0,0 +1,13 @@
+//
+//  RequestParameters.swift
+//  Weather
+//
+//  Created by Vladislav Horbenko on 27.06.2020.
+//  Copyright © 2020 VH. All rights reserved.
+//
+
+import Foundation
+
+struct RequestParameters {
+    <#fields#>
+}
---
+++ Weather/Weather/Services/NetworkService.swift
@@ -0,0 +1,21 @@
+//
+//  NetworkService.swift
+//  Weather
+//
+//  Created by Vladislav Horbenko on 27.06.2020.
+//  Copyright © 2020 VH. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+enum Result<T> {
+  case success(T)
+  case error(Error?)
+}
+
+class NetworkService {
+   
+    
+    
+}
